dd8e325ab1976b884e088fb4906cfaefe0197c9c
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-04 03:12:24 +0800

    yep

--- multisound/lib/main.dart
+++ multisound/lib/main.dart
@@ -6,23 +6,7 @@ void main() => runApp(MaterialApp(home: Scaffold(body: Xylophone())));
 class Xylophone extends StatelessWidget {
   const Xylophone({Key? key}) : super(key: key);
 
-  void playSound(int soundNumber) async {
-    final player = AudioPlayer(); // create a new instance
-    await player.play(AssetSource('$soundNumber.mp3'));
-  }
 
-  Expanded buildKey({required int soundNumber, required Color color}) {
-    return Expanded(
-      child: TextButton(
-        child: const Text(''),
-        onPressed: () {
-          playSound(soundNumber);
-        },
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(color)),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,3 +20,92 @@ class Xylophone extends StatelessWidget {
     ));
   }
 }
+
+
+
+
+Expanded buildKey({required int soundNumber, required Color color}) {
+    return Expanded(
+      child: TextButton(
+        child: const Text(''),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(color)),
+      ),
+    );
+  }
+
+
+
+
+  void playSound(int soundNumber) async {
+    final player = AudioPlayer(); // create a new instance
+    await player.play(AssetSource('$soundNumber.mp3'));
+  }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
+
+
+// void main() => runApp(MaterialApp(home: Scaffold(body: Xylophone())));
+
+
+// class Xylophone extends StatelessWidget {
+//  const Xylophone({Key? key}) : super(key: key);
+//  @override
+//  Widget build(BuildContext context) {
+//    return SafeArea(
+//        child: Column(
+//      crossAxisAlignment: CrossAxisAlignment.stretch,
+//      children: [
+//        buildKey(soundNumber: 1, color: Colors.red),
+//        buildKey(soundNumber: 2, color: Colors.orange),
+
+//      ],
+//    ));
+//  }
+// }
+
+
+// final player = AudioPlayer();
+// void playSound(int soundNumber) async {
+//   await player.play(AssetSource('$soundNumber.mp3'));
+// }
+
+
+
+// final player2 = AudioPlayer();
+// void playSound2(int soundNumber) async {
+//   await player.play(AssetSource('$soundNumber.mp3'));
+// }
+
+// Expanded buildKey({required int soundNumber, required Color color}) {
+//  return Expanded(
+//    child: TextButton(
+//      child: Text(''),
+//      onPressed: () {
+//        playSound(soundNumber);
+//      },
+//      style:
+//          ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color)),
+//    ),
+//  );
+// }
+
+
+// Expanded buildKey2({required int soundNumber, required Color color}) {
+//  return Expanded(
+//    child: TextButton(
+//      child: Text(''),
+//      onPressed: () {
+//        playSound2(soundNumber);
+//      },
+//      style:
+//          ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color)),
+//    ),
+//  );
+// }
---
+++ multisound/lib/thisalsoworks.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+
+void main() => runApp(MaterialApp(home: Scaffold(body: Xylophone())));
+
+
+class Xylophone extends StatelessWidget {
+ const Xylophone({Key? key}) : super(key: key);
+
+
+ @override
+ Widget build(BuildContext context) {
+   return SafeArea(
+       child: Column(
+     crossAxisAlignment: CrossAxisAlignment.stretch,
+     children: [
+       buildKey(soundNumber: 1, color: Colors.red),
+       buildKey2(soundNumber: 2, color: Colors.orange),
+
+     ],
+   ));
+ }
+}
+
+
+  final player = AudioPlayer();
+void playSound(int soundNumber) async {
+
+  await player.play(AssetSource('$soundNumber.mp3'));
+}
+
+
+
+  final player2 = AudioPlayer();
+void playSound2(int soundNumber) async {
+
+  await player2.play(AssetSource('$soundNumber.mp3'));
+}
+
+Expanded buildKey({required int soundNumber, required Color color}) {
+ return Expanded(
+   child: TextButton(
+     child: Text(''),
+     onPressed: () {
+       playSound(soundNumber);
+     },
+     style:
+         ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color)),
+   ),
+ );
+}
+
+
+Expanded buildKey2({required int soundNumber, required Color color}) {
+ return Expanded(
+   child: TextButton(
+     child: Text(''),
+     onPressed: () {
+       playSound2(soundNumber);
+     },
+     style:
+         ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(color)),
+   ),
+ );
+}
